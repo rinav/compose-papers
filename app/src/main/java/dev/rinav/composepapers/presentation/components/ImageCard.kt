be49054b46d33d1c042b3b42d632ab67c9b62271
Rinav Gangar <rinav4all@gmail.com>
2025-08-04 21:07:55 +0530

    Update ImageCard with click effect

--- app/src/main/java/dev/rinav/composepapers/presentation/components/ImageCard.kt
+++ app/src/main/java/dev/rinav/composepapers/presentation/components/ImageCard.kt
@@ -1,5 +1,7 @@
 package dev.rinav.composepapers.presentation.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,39 +11,62 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import dev.rinav.composepapers.domain.models.PaperImage
+import kotlinx.coroutines.delay
 
 @Composable
 fun ImageCard(
     modifier: Modifier = Modifier,
     image: PaperImage,
     loadThumbnail: Boolean,
-    contentScale:ContentScale,
+    contentScale: ContentScale,
     isFavorite: Boolean,
     onItemClick: (PaperImage) -> Unit,
     onToggleFavStatus: () -> Unit
 ) {
     val aspectRatio: Float by remember {
-        derivedStateOf { (image?.width?.toFloat() ?: 1f) / (image?.height?.toFloat() ?: 1f) }
+        derivedStateOf { image.width.toFloat() / image.height.toFloat() }
     }
+    var isClicked by remember { mutableStateOf(false) }
+    val scale = animateFloatAsState(
+        targetValue = if (isClicked) 0.95f else 1f, // Slightly smaller on click
+        animationSpec = tween(durationMillis = 100)
+    )
+
+    // LaunchedEffect *outside* the clickable modifier
+    LaunchedEffect(isClicked) { // Key on isClicked
+        if (isClicked) {
+            delay(100) // Match animation duration
+            isClicked = false // Reset the color
+            onItemClick(image)
+        }
+    }
+
     Card(
         shape = RoundedCornerShape(16.dp),
         modifier = Modifier
             .fillMaxWidth()
             .aspectRatio(aspectRatio)
+            .scale(scale.value) // Apply scale change,
             .then(modifier),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surfaceVariant,
         ),
         elevation = CardDefaults.cardElevation(defaultElevation = 0.dp, pressedElevation = 0.dp),
-        onClick = { onItemClick(image) },
+        onClick = {
+            isClicked = true
+        },
     ) {
         Box(modifier = Modifier) {
             AsyncPaperImage(
